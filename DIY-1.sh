1db73bd7c2a42c87602e11e9e939e180c0556a65
virkingz <68280845+virkingz@users.noreply.github.com>
2026-03-04 14:17:29 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -5,7 +5,7 @@
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
 # 2-添加 PowerOff 关机插件
-git clone https://github.com/WukongMaster/luci-app-poweroff.git package/luci-app-poweroff
+# git clone https://github.com/WukongMaster/luci-app-poweroff.git package/luci-app-poweroff
 
 # 3-添加 opentomcat 主题
 # git clone https://github.com/WukongMaster/luci-theme-opentomcat.git package/luci-theme-opentomcat
@@ -14,6 +14,12 @@ git clone https://github.com/WukongMaster/luci-app-poweroff.git package/luci-app
 # sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.conf.default
 
 # 5-添加vnt插件
+# Uncomment a feed source
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+sed -i '1i src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' feeds.conf.default
+sed -i '2i src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' feeds.conf.default
 git clone https://github.com/lmq8267/luci-app-vnt.git package/vnt
 
 # 5-添加 PassWall 插件
@@ -21,8 +27,8 @@ git clone https://github.com/lmq8267/luci-app-vnt.git package/vnt
 # echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 
 # 添加外部插件
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
-#sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+# sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+# sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 
 

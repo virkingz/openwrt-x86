e25f12954c83ce33a12c2c47eb7966f5e82da8fa
virkingz <68280845+virkingz@users.noreply.github.com>
2026-06-15 10:27:34 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -2,7 +2,7 @@
 # =======================================================================================================================
 
 # 1-添加 ShadowSocksR Plus+ 插件
-# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
 # 2-添加 PowerOff 关机插件
 # git clone https://github.com/WukongMaster/luci-app-poweroff.git package/luci-app-poweroff
@@ -21,10 +21,6 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 sed -i '1i src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' feeds.conf.default
 sed -i '2i src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' feeds.conf.default
 git clone https://github.com/lmq8267/luci-app-vnt.git package/vnt
-
-# 5-添加 PassWall 插件
-# echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
-# echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 
 # 添加外部插件
 # sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default

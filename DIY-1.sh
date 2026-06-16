c6a1fd0475a74a85c6781fad4c693aa38d1732c1
virkingz <68280845+virkingz@users.noreply.github.com>
2026-06-16 09:47:38 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -1,30 +1,12 @@
 # DIY-1 此脚本功能：添加外部插件
 # =======================================================================================================================
-
-# 1-添加 ShadowSocksR Plus+ 插件
-# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-
-# 2-添加 PowerOff 关机插件
-# git clone https://github.com/WukongMaster/luci-app-poweroff.git package/luci-app-poweroff
-
-# 3-添加 opentomcat 主题
-# git clone https://github.com/WukongMaster/luci-theme-opentomcat.git package/luci-theme-opentomcat
-
-# 4-添加 OpenClash 插件
-# sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.conf.default
-
-# 5-添加vnt插件
-# Uncomment a feed source
-# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
-# sed -i '1i src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' feeds.conf.default
-# sed -i '2i src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' feeds.conf.default
+# 1-添加vnt插件
 git clone https://github.com/lmq8267/luci-app-vnt.git package/vnt
 
 # 添加外部插件
-# sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-# sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
-# sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
-
-
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,sing*,smartdns} feeds/packages/utils/v2dat feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang -b 1.26 feeds/packages/lang/golang
+./scripts/feeds install -a
